03ad95895217b49c6ffb68df7f5af144c895de93
Yassin Makrai <makrai.yassin@gmail.com>
2022-09-19 13:52:57 +0200

    first commit

---
+++ src/main/java/be/digitalcity/projetspringrest/ProjetSpringRestApplication.java
@@ -0,0 +1,13 @@
+package be.digitalcity.projetspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSpringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetSpringRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/digitalcity/projetspringrest/ProjetSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package be.digitalcity.projetspringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSpringRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
