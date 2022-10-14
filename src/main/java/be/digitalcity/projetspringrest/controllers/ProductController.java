ff55caac021873857312530c8ac175c4cf85b45c
Yassin Makrai <70712131+Makraiyassin@users.noreply.github.com>
2022-10-14 12:18:40 +0200

    Develop (#20)
    * mappers + test
    
    * fix getters setters lombok (#1)
    
    * Feature register (#2)
    
    * product controller/service/mapper
    
    * save progress for register + logic of jwt (jwt filter/provider/properties + token dto + security config + User controler/mapper/repository/service)
    
    * register + restructuration des table pour évité les appel circulaire
    
    * login form + controller (#3)
    
    * data init + CRUD (#4)
    
    * verif si l'addresse existe deja en db + verif si l'utilisateur existe (#5)
    
    * swagger config , user controller profil, user servic getUserWithToken
    
    * Fix user profil (#6)
    
    * suppression dependance inutiles
    
    * application.yml
    
    * fonctionnalité role à fixer !
    
    * fix getUser
    
    * Feature create omnitheque (#7)
    
    * suppression dependance inutiles
    
    * application.yml
    
    * fonctionnalité role à fixer !
    
    * fix getUser
    
    * create omnitheque + update user
    
    * fix user
    
    * Feature crud product (#9)
    
    * add product ok
    
    * OK sauf Delete => Fixer les delete en cascade ....
    
    * refactor product form
    
    * fix crud omnitheque
    
    * fix ajout d'un produit => check produits deja existant mais quantity = 0
    
    * suppression import/code inutilisé
    
    * Develop (#8) (#11)
    
    * mappers + test
    
    * fix getters setters lombok (#1)
    
    * Feature register (#2)
    
    * product controller/service/mapper
    
    * save progress for register + logic of jwt (jwt filter/provider/properties + token dto + security config + User controler/mapper/repository/service)
    
    * register + restructuration des table pour évité les appel circulaire
    
    * login form + controller (#3)
    
    * data init + CRUD (#4)
    
    * verif si l'addresse existe deja en db + verif si l'utilisateur existe (#5)
    
    * swagger config , user controller profil, user servic getUserWithToken
    
    * Fix user profil (#6)
    
    * suppression dependance inutiles
    
    * application.yml
    
    * fonctionnalité role à fixer !
    
    * fix getUser
    
    * Feature create omnitheque (#7)
    
    * suppression dependance inutiles
    
    * application.yml
    
    * fonctionnalité role à fixer !
    
    * fix getUser
    
    * create omnitheque + update user
    
    * fix user
    
    * refactor changemenent structure entités product et omnitheque ManyToMany => oneToMany
    
    * restructure table role et user_roles
    
    * Feature borrow (#13)
    
    * error dto + controller advisor => gestion d'errur element not fond exception
    
    * creation d'un emprunt + gestion d'erreur (controller advisor)
    
    * modif structur table borrow et product
    
    * retour d'emprunt + verifications + gestion d'erreur
    
    * version soft du delete product (hard delete fonctionne pas donc delete = quantity 0)
    
    * borrow terminer + refactor methode create product + restructure shemas db oneToMany et manyToOne
    
    * feature shearch
    
    * fixe description varchar 2000
    
    * Feature image (#15)
    
    * save => fixe front-end
    
    * fix add exist product
    
    * fix & refactor toList => collectors.toList() , isEmptu => !isPresent
    
    * image in update omni
    
    * fix nullPointerException in productMapper
    
    * include description in search function
    
    * appliction.yml
    
    * Feature post (#17)
    
    * post controller
    
    * verification delete post + cascade
    
    * to fix delete post only from owner
    
    * Feature post (#18)
    
    * post controller
    
    * verification delete post + cascade
    
    * to fix delete post only from owner
    
    * changement taille content post . Feature post terminer => à fixer : verification que l'omni possede bien le post à supprimer avant de le faire
    
    * application.yml afficher pour le deploiement
    
    * config CORS

--- src/main/java/be/digitalcity/projetspringrest/controllers/ProductController.java
+++ src/main/java/be/digitalcity/projetspringrest/controllers/ProductController.java
@@ -11,7 +11,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:4200/"})
+@CrossOrigin(origins = {"http://localhost:4200/","https://makraiyassin.github.io/"})
 @RequestMapping("/api/product")
 public class ProductController {
     private final ProductService service;
